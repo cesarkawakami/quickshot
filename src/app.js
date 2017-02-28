5227c1365438b3b0561bf81af5d07f6531bc40a2
Cesar Kawakami <cesarkawakami@gmail.com>
2017-02-28 03:56:45 -0300

    Adding flow

---
+++ src/app.js
@@ -0,0 +1,15 @@
+// @flow
+
+import React from 'react';
+
+export default class App extends React.Component {
+  render() {
+    const x : String = 5;
+    console.log(x);
+    return (
+      <div>
+        <h2>Welcome to React!</h2>
+      </div>
+    );
+  }
+}
